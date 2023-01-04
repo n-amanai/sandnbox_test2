fac5e23d2b145e18c64531a6e4da0c1f0a874e68
天内和 <n-amanai@bridge.co.jp>
2023-01-04 06:57:21 +0000

    Update 14 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+/**
+ * const、let等の宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
+
+// var変数を上書き
+val1 = "val変数の上書き";
+console.log(val1);
