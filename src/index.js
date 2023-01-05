1d0d218c9f005d4a74167f568b0b1bac2a5594ff
n-amanai <121922562+n-amanai@users.noreply.github.com>
2023-01-05 13:36:22 +0900

    モダンjavascript完了

--- src/index.js
+++ src/index.js
@@ -126,24 +126,120 @@
  */
 
 // 配列の展開
-const arr1 = [1, 2];
+// const arr1 = [1, 2];
 //  console.log(arr1);
 //  console.log(...arr1);
 
-const sumFunc = (num1, num2) => console.log(num1 + num2);
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
 // 従来
 //  sumFunc(arr1[0],arr1[1]);
 // スプレッド構文
-sumFunc(...arr1);
+// sumFunc(...arr1);
 
 // まとめる
-const arr2 = [1, 2, 3, 4, 5];
+// const arr2 = [1, 2, 3, 4, 5];
 // 従来
 // const [num1, num2, num3, num4, num5] = arr2;
 // スプレッド構文
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
 
-// 配列のコピー、結合
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// 参照になってしまうため、非推奨
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] =
+
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "じゃけぇ"];
+// // 従来の方法
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+// // mapを使用した配列の処理(returnに基づいて変数を格納）
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// // mapを活用した配列の処理（処理のみ）
+// nameArr.map((name) => console.log(name));
+// インデックスを取得するには？
+// nameArr.map((name, index) => console.log(`${index+1}番目は${name}です`));
+// const numArr = [1, 2, 3, 4, 5];
+
+// filterを使用した値の選別
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 10));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2){
+//   console.log("1か2はtrueになります");
+// }
+// if(flag1 && flag2){
+//   console.log("1も2もtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
